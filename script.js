cdfbdfe4f416ec70d02081933b3579bc04268214
Maxwel Kiprotich <kiprotichmaxwel5@gmail.com>
2025-09-03 12:36:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+let number = 10;
+if (number % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+let age = 18;
+if (age >= 18) {
+    console.log("Eligible to vote");
+} else {
+    console.log("Is not eligible to vote");
+}
+
+let digit = 2;
+if (digit > 0) {
+    console.log("positive");
+} else if (digit < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
+
+let number1 = 50;
+if (number1 % 5 === 0 ){
+    console.log("Divisible by 5");
+} else {
+    console.log("Not divisible by 5");
+}
+
+let marks = 85;
+if (marks >= 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+let password = "python123";
+if (password === "python123") {
+    console.log("Accesss granted");
+} else {
+    console.log("access Denied");
+}
+
+let year = 2020;
+if (year % 4 === 0 ) {
+    console.log("Leap year");
+} else {
+    console.log("Not a leap year");
+}
+
+let studentage = 17;
+if (studentage >= 13 &&  studentage <= 19) {
+    console.log("Person is a teenager");
+}
+else{
+    console.log("Person is not a teenager");
+}
+
+let character = "a";
+if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u') {
+    console.log("Character is a vowel");
+}
+else {
+    console.log("Character is a consonant");
+}
+
+let number2 = 101;
+if (number2 > 100){
+    console.log("Number is greater than 100");
+}
+else {
+    console.log("Number is not greater than 100");
+}
