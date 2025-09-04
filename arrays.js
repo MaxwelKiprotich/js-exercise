850c00996e3d223393571d8a0caa26de38637a87
Maxwel Kiprotich <kiprotichmaxwel5@gmail.com>
2025-09-04 13:25:50 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,21 @@
+///Definition: An arrary is a collection of elements stored in contigous memory location.
+///Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+//Creating an array
+let fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits); // Output: ["Apple", "Banana", "Orange"]
+
+//Accessing elements in an array
+console.log(fruits[0]); // Output: Apple
+console.log(fruits[1]); // Output: Banana
+
+//Modifying elements in an array
+fruits[2] = "Grapes";
+console.log(fruits); // Output: ["Apple", "Banana", "Grapes"]
+
+
+let number = [1, 2, 3, 4, 5];
+console.log(number[0] + number[1] + number[2] + number[3] + number[4]);
+
+let lNumber = [10 , 25, 7, 30, 15];
+console.log
